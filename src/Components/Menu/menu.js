d53655115a17d51918c40cf944957564f470fa54
Andrew McMillion <amcmillion3@gmail.com>
2022-06-04 15:21:54 -0400

    Add Menu Info

---
+++ src/Components/Menu/menu.js
@@ -0,0 +1,364 @@
+import React from "react";
+
+function Menu() {
+
+    return(
+        <div>
+            <div>
+                <h2>APERITIVOS</h2>
+                <div>
+                    <h4>Fiesta Catracha</h4>
+                    <p>Tacos de pollo, baleada sencilla, empanadas, tajadas de guineo, friojoles fritos, queso fresco, y chimol</p>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h4>Tacos de Pollo</h4>
+                            <p>Honduran style chicken tacos, cabbage, tomato sauce, crumbled cheese, and chimol</p>
+                        </li>
+                        <li>
+                            <h4>Enchiladas</h4>
+                            <p>3 Fried corn tortilla, ground beef, cabbage, house sauce, crumbled cheese, chimol</p>
+                        </li>
+                        <li>
+                            <h4>Tortilla con Quesillo</h4>
+                            <p>Corn tortilla cheese quesadilla, cabbage, tomato sauce, crumbled cheese, and chimol</p>
+                        </li>
+                        <li>
+                            <h4>Catracha</h4>
+                            <p>Fried corn tortilla, refried beans, and crumbled cheese</p>
+                        </li>
+                        <li>
+                            <h4>Papa Rellena</h4>
+                            <p>Homemade Portato ball stuffed with ground beef</p>
+                        </li>
+                        <li>
+                            <h4>Relleno de Cangrejo</h4>
+                            <p>Homemade Deviled Crab</p>
+                        </li>
+                        <li>
+                            <h4>Boquita Típica</h4>
+                            <p>Carne asada, fried green bananas, fried cheese, chimol</p>
+                        </li>
+                        <li>
+                            <h4>Croquetas</h4>
+                            <p>Fried Spanish Croquettes</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <h2>SOPAS Y ENSALADAS</h2>
+                <div>
+                    <h4>Sopa de Res</h4>
+                    <p>Homemade beef soup using Abuelitas delicious recipe (Todos los días)</p>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h4>Sopa de Pollo</h4>
+                            <p>Gomemade Chicken Soup (Miércoles y Jueves)</p>
+                        </li>
+                        <li>
+                            <h4>Sopa de Gallina</h4>
+                            <p>Homemade Hen Soup (Lunes)</p>
+                        </li>
+                        <li>
+                            <h4>Sopa de Frijoles</h4>
+                            <p>Homemade Red Bean Soup (Viernes)</p>
+                        </li>
+                        <li>
+                            <h4>Sopa de Mondongo</h4>
+                            <p>Homemade Tripe Soup (Sabados)</p>
+                        </li>
+                        <li>
+                            <h4>Sopa de Caracol</h4>
+                            <p>Homemade Conch Soup (Domingos)
+                                *Traditional Honduran Chowder
+                            </p>
+                        </li>
+                        <li>
+                            <h4>Catracha Chef Salad</h4>
+                            <p>Lettuce, tomato, shredded cheese, hard-boiled egg, sliced ham, and sliced turkey</p>
+                        </li>
+                        <li>
+                            <h4>Ensalada con Pechuga a la Plancha</h4>
+                            <p>Grilled chicken, lettuce, tomato, onions, cucumbers, fresh cheese, and avocado</p>
+                        </li>
+                        <li>
+                            <h4>Ensalada con Camarones a la Plancha</h4>
+                            <p>Grilled shrimp, lettuce, tomato, grilled onions, cucumbers, and avocado</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <h2>SÁNDWICHES</h2>
+                <div>
+                    <ul>
+                        <li>
+                            <h4>Sándwich Cubano</h4>
+                            <p>Ham, salami, roast pork, swiss cheese, mayonnaise, mustard, and pickles</p>
+                        </li>
+                        <li>
+                            <h4>Media Noche</h4>
+                            <p>Ham, salami, roast pork, swiss cheese, mayonnaise, mustard, and pickles</p>
+                        </li>
+                        <li>
+                            <h4>Sándiwch de Bistec</h4>
+                            <p>Steak, grilled onions, mayonnaise, lettuce, tomato, and potato sticks</p>
+                        </li>
+                        <li>
+                            <h4>Sándiwch de Puerco</h4>
+                            <p>Roast pork, grilled onions, mayonnaise, lettuce, tomato, and potato sticks</p>
+                        </li>
+                        <li>
+                            <h4>Doña Rossina Sándiwch</h4>
+                            <p>Ham, turkey, bacon, swiss cheese, mayonnaise, lettuce, and tomato</p>
+                        </li>
+                        <li>
+                            <h4>Pan con Croqueta</h4>
+                            <p>Ham croquettes, sliced hame, swiss cheese, mayonnaise, mustard, and ketchup</p>
+                        </li>
+                    </ul>
+                    <div>
+                        <h4>La Casa Catracha Sándiwch</h4>
+                        <p>House chicken, mayonnaise, lettuce, tomato, and French Fries</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h2>DESAYUNOS</h2>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>Desayuno Típico</h4>
+                                <p>Traditional Honduran breakfast. 2 Eggs, refried beans, Honduran cheese, crema, and tortillas
+                                    Add carne asada and avocado
+                                </p>
+                            </li>
+                            <li>
+                                <h4>Baleadas</h4>
+                                <p>Flour tortilla folded in half filled with breakfast items</p>
+                                <ul>
+                                    <li>
+                                        <h5>Sencilla</h5>
+                                        <p>Refried beans, crema, and crumbled cheese</p>
+                                    </li>
+                                    <li>
+                                        <h5>Con Huevo</h5>
+                                        <p>Refried beans, crema, crumbled cheese, and scrambled eggs</p>
+                                    </li>
+                                    <li>
+                                        <h5>Con Todo</h5>
+                                        <p>Refried beans, crema, crumbled cheese, scrambled eggs, carne asada, and avocado</p>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h2>ALMUERZO Y CENA</h2>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>Pargo Rojo Frito</h4>
+                                <p>Fried Red Snapper served with 3 side items.</p>
+                            </li>
+                            <li>
+                                <h4>Camarones Enchilados</h4>
+                                <p>Shrimp served in a spicy sauce with 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Camarones al Ajillo</h4>
+                                <p>Shrimp served in a garlic sauce with 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Camarones a la Plancha</h4>
+                                <p>Grilled Shrimp accompanied by 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Camarones Empanizados</h4>
+                                <p>Breaded Shrimp accompanied by 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Filete de Pescado</h4>
+                                <p>Tilapia Fillet accompanied by 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Carne Asada</h4>
+                                <p>Flame grilled steak served with 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Carnes a la Plancha</h4>
+                                <p>Your choice of meat accompanied by 3 side items
+                                    Pechuga | Bistec | Chuleta
+                                </p>
+                            </li>
+                            <li>
+                                <h4>Carnes Encebolladas</h4>
+                                <p>Your choice of meat and grilled onions with 3 side items
+                                    Pechuga | Bistec | Chuleta
+                                </p>
+                            </li>
+                            <li>
+                                <h4>Carnes Guisadas</h4>
+                                <p>Meats stewed in tomato sauce, onion, and green peppers
+                                    Pechuga | Bistec | Chuleta
+                                </p>
+                            </li>
+                            <li>
+                                <h4>Pollo Frito</h4>
+                                <p>Fried Chicken accompanied by 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Puerco Asado</h4>
+                                <p>Roast Pork accompanied by 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Carne Molida | Picadillo</h4>
+                                <p>Seasoned ground beef accompanied by 3 side items</p>
+                            </li>
+                            <li>
+                                <h4>Pollo Fricase</h4>
+                                <p>Stewed chicken and potatoes accompanied by your choice of 3 side items</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h2>PLATOS HONDUREÑOS</h2>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>Yuca con Chicharrón</h4>
+                                <p>Cassava, pork rinds, cabbage, tomato sauce, and chimol</p>
+                            </li>
+                            <li>
+                                <h4>Yuca con Pata</h4>
+                                <p>Cassava, pig feet, cabbage, tomato sauce, and chimol</p>
+                            </li>
+                            <li>
+                                <h4>Pastelitos</h4>
+                                <p>3 Corn empanadas filled with ground beef and rice served with cabbage, red sauce, and chimol</p>
+                            </li>
+                            <li>
+                                <h4>Tajadas al Estilo Ceiba</h4>
+                                <p>Fried green bananas, cabbage, red sauce, house dressing, encurtido your choice of meat
+                                    Pollo Frito | Carne Asada | Chuleta | Carne Molida
+                                </p>
+                            </li>
+                            <li>
+                                <h4>Tajadas al Estilo Honduras</h4>
+                                <p>Fried green bananas, cabbage, red sauce, chimol, and your choice of meat
+                                    Pollo Frito | Carne Asada | Chuleta | Carne Molida
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h2>ACOMPAÑANTES</h2>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>Arroz</h4>
+                                <p>White or Yello Rice</p>
+                            </li>
+                            <li>
+                                <h4>Tostones | Maduros</h4>
+                                <p>Fried green or Sweet Plantain</p>
+                            </li>
+                            <li>
+                                <h4>Frijoles</h4>
+                                <p>Red, Black, or Refried</p>
+                            </li>
+                            <li>
+                                <h4>Papas Fritas</h4>
+                                <p>French Fries</p>
+                            </li>
+                            <li>
+                                <h4>Tajadas de Guineo</h4>
+                                <p>Fried Green Banasas</p>
+                            </li>
+                            <li>
+                                <h4>Queso Fresco</h4>
+                                <p>Fresh Honduran Cheese</p>
+                            </li>
+                            <li>
+                                <h4>Ensalada de Repollo | Lechuga</h4>
+                                <p>Side Cabbage or Lettuce Salad</p>
+                            </li>
+                            <li>
+                                <h4>Aguacate</h4>
+                                <p>Sliced Avocado</p>
+                            </li>
+                            <li>
+                                <h4>Yuca Frita | Hervida</h4>
+                                <p>Cassava Fried or Boiled</p>
+                            </li>
+                            <li>
+                                <h4>Totillas Hechas a Mano</h4>
+                                <p>Handmade corn or flour tortillas</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h2>POSTRES</h2>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>Arroz con Leche</h4>
+                                <p>Rice Pudding</p>
+                            </li>
+                            <li>
+                                <h4>Tres Leche</h4>
+                                <p>3 Milk Cake</p>
+                            </li>
+                            <li>
+                                <h4>Flan de Vainilla</h4>
+                                <p>Vanilla Flan</p>
+                            </li>
+                            <li>
+                                <h4>Taco Dulce</h4>
+                                <p>Sweet taco, vanilla ice cream, whipped cream, and chocolate syrup</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h2>BEBIDAS</h2>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>Judos Naturales</h4>
+                                <p>Natural fruit juices</p>
+                            </li>
+                            <li>
+                                <h4>Café Cubano</h4>
+                                <p>Cuban Coffee</p>
+                            </li>
+                            <li>
+                                <h4>Sodas Latinas</h4>
+                                <p>Latin Sodas</p>
+                            </li>
+                            <li>
+                                <h4>Café con Leche</h4>
+                                <p>Coffee with milk</p>
+                            </li>
+                            <li>
+                                <h4>Sodas de Maquina</h4>
+                                <p>Fountain Drink</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
---
+++ src/Components/Nav/nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+    
+    return(
+        <nav>
+            <div>La Casa Catracha Logo</div>
+            <div>
+                <div><a href="#menu">Menu</a></div>
+                <div><a href="#about">About</a></div>
+                <div><a href="#contact">Contact</a></div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
