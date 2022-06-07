1164ca22c65ea47511ee6495f87c476dea840735
Andrew McMillion <amcmillion3@gmail.com>
2022-06-06 22:19:08 -0400

    Add updated routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './Components/Nav';
+import HomeSlider from './Components/Home';
+import Menu from './Components/Menu';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Router>
+        <Routes>
+        <Route path='/' element={<HomeSlider />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
